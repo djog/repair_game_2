c48064f4cf1b01989e68851d662e508edb9283c0
noobie981 <nicolai@richelbilderbeek.nl>
2022-07-07 21:05:36 +0200

    removed block at victory screen (+ WIP victory screen shutdown)

--- src/minigame.js
+++ src/minigame.js
@@ -6,13 +6,12 @@ class Minigame {
     onStop() { };
 }
 
-const MINIGAMES = { "GAME": 0, "BOLTS": 1, "NUMBERS" : 2, "KEYGAME" : 3, "BOXGAME" : 4 }
+const MINIGAMES = { "GAME": 0, "BOLTS": 1, "NUMBERS": 2, "KEYGAME": 3, "BOXGAME": 4 }
 Object.freeze(MINIGAMES);
 
-class GameState{
+class GameState {
 
-    constructor(wonGame, game)
-    {
+    constructor(wonGame, game) {
         this.wonGame = wonGame;
         this.game = game;
     }
@@ -27,19 +26,18 @@ class StateManager {
     }
 
     buildMinigame(state) {
-        if (state.wonGame){
+        if (state.wonGame) {
             this.wonGames.push(this.currentMinigame.game);
             const allGames = Object.values(MINIGAMES).slice(1);
             let hasWon = true;
             debugger;
             for (let game of allGames) {
-                if (!this.wonGames.includes(game))
-                {
+                if (!this.wonGames.includes(game)) {
                     hasWon = false;
                     break;
                 }
             }
-            this.victory = hasWon;      
+            this.victory = hasWon;
         }
         switch (state.game) {
             case MINIGAMES.GAME:
@@ -63,19 +61,17 @@ class StateManager {
     switchState(state) {
         const newState = this.buildMinigame(state);
 
-        if (newState)
-        {
+        if (newState) {
             this.currentMinigame.onStop();
             this.currentMinigame = newState;
             this.currentMinigame.onStart(this);
         }
     }
 
-    
+
     updateFrame() {
 
-        if (this.victory)
-        {
+        if (this.victory && this.victoryScreenShown == false) {
             // Draw
             push();
 
@@ -89,25 +85,26 @@ class StateManager {
             text("Congratulations!", width / 2 - 250, height / 3);
             text("You have completed all levels!", width / 2 - 420, height / 3 + 100);
             text("Click here to return to the ship", width / 2 - 420, height / 3 + 320);
-            if (mouseX < width / 2 + 500 && 
-                mouseX > width / 2 - 500 && 
-                mouseY < height / 3 + 400 && 
+            debugger;
+            if (mouseX < width / 2 + 500 &&
+                mouseX > width / 2 - 500 &&
+                mouseY < height / 3 + 400 &&
                 mouseY > height / 3 + 200) {
-                rect(width - 490, height / 3 + 210, 980, 180)
                 if (mouseIsPressed) {
                     this.victory = false;
+                    this.victoryScreenShown = true;
                 }
             }
-    
+
             pop();
 
-        } else{
+        } else {
             // Update
             let newState = this.currentMinigame.onUpdate(deltaTime / 1000);
-            if (newState != null) {        
+            if (newState != null) {
                 this.switchState(newState);
-            } 
-            
+            }
+
             // Draw
             push();
             this.currentMinigame.onDraw();
